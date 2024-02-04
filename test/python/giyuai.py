29730c8deea2616b11319c510a6dd9b588cb0d48
matsurix28@Rapport <matsurix28@gmail.com>
2024-02-04 11:41:35 +0000

    canvas

---
+++ test/python/giyuai.py
@@ -0,0 +1,19 @@
+from detect import Detect
+import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
+
+img = cv2.imread('test/output/daen/1.png')
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img_pil = Image.fromarray(img_rgb)
+print(img_pil.size)
+
+
+root = tk.Tk()
+root.title('title dayo')
+root.geometry('800x450')
+canvas = tk.Canvas(root, width=img.shape[1], height=img.shape[0])
+canvas.pack()
+img_tk = ImageTk.PhotoImage(img_pil)
+canvas.create_image(0,0, image=img_tk, anchor='nw')
+root.mainloop()
