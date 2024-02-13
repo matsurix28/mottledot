5a02fcdd2500d87d09e67439dba074391616f5e7
matsurix28@Rapport <matsurix28@gmail.com>
2024-02-13 08:14:24 +0000

    select box

--- test/python/gui.py
+++ test/python/gui.py
@@ -45,28 +45,84 @@ class MainFrame(ttk.Frame):
 class DetectFrame(ttk.Frame):
     def __init__(self, master):
         super().__init__(master, relief='groove', borderwidth=10)
-        img_frm = self._img_area()
-        img_frm.pack(fill='both',expand=True)
+        self.thresh = tk.StringVar()
+        self.min_h = tk.StringVar()
+        self.max_h = tk.StringVar()
+        img_frm = self._image_frame()
+        method_frm = self._method_frame()
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        img_frm.propagate(0)
+        method_frm.propagate(0)
+        img_frm.grid(row=0, column=0, sticky='NSEW')
+        method_frm.grid(row=0, column=1, sticky='NSEW')
 
-
-    def _img_area(self):
+    def _image_frame(self):
         img_frm = ttk.Frame(self)
         before_frm = ImageFrame(img_frm)
         img = Image.open('src/arrow.png')
         img = img.resize((48,48))
         self.img = ImageTk.PhotoImage(img)
+        space_frm = ttk.Frame(img_frm, height=30)
         arrow = tk.Label(img_frm, image=self.img)
         after_frm = ImageFrame(img_frm, out=True)
-        img_frm.grid_columnconfigure(0, weight=5)
-        img_frm.grid_columnconfigure(1, weight=1)
-        img_frm.grid_columnconfigure(2, weight=5)
-        img_frm.grid_rowconfigure(0, weight=1)
+        img_frm.grid_columnconfigure(0, weight=1)
+        img_frm.grid_rowconfigure(0, weight=5)
+        img_frm.grid_rowconfigure(3, weight=5)
         before_frm.grid(row=0, column=0, sticky='NSEW')
-        arrow.grid(row=0, column=1, sticky='NSEW')
-        after_frm.grid(row=0, column=2, sticky='NSEW')
+        space_frm.grid(row=1, column=0)
+        arrow.grid(row=2, column=0)
+        after_frm.grid(row=3, column=0, sticky='NSEW')
         before_frm.propagate(0)
         after_frm.propagate(0)
         return img_frm
+    
+    def _method_frame(self):
+        method_frm = tk.Frame(self)
+        methods = ('Detect contours', 'Extract Green area')
+        select_box = ttk.Combobox(method_frm, state='readonly', values=methods, font=('Calibri', 12))
+        select_box.set('Detect contours')
+        select_box.bind('<<ComboboxSelected>>', self._select_method)
+        cnt_frm = self._contour_method_frame(method_frm)
+        grn_frm = self._green_method_frame(method_frm)
+        method_frm.grid_columnconfigure(0, weight=1)
+        method_frm.grid_rowconfigure(0, weight=1)
+        method_frm.grid_rowconfigure(1, weight=10)
+        cnt_frm.propagate(0)
+        grn_frm.propagate(0)
+        select_box.grid(row=0, column=0)
+        cnt_frm.grid(row=1, column=0, sticky='NSEW')
+        grn_frm.grid(row=1, column=0, sticky='NSEW')
+        cnt_frm.tkraise()
+        return method_frm
+    
+    def _contour_method_frame(self, master):
+        self.cnt_frm = tk.Frame(master, relief='groove', borderwidth=5, bg='red')
+        #img = Image.open('src/cnts.png')
+        #self.cnts_img = ImageTk.PhotoImage(img)
+        ex_img = tk.Label(self.cnt_frm,  height=10, width=50, bg='red')
+        val_frm = tk.Frame(self.cnt_frm, height=50, bg='blue')
+        thresh_entry = ttk.Entry(val_frm, textvariable=self.thresh)
+        up_btn = ttk.Button(val_frm, text='▲')
+        down_btn = ttk.Button(val_frm, text='▼')
+        run_btn = ttk.Button(self.cnt_frm, text='RUN')
+        name_lbl.pack()
+        ex_img.pack()
+        val_frm.pack(fill='x')
+        run_btn.pack()
+        return self.cnt_frm
+    
+    def _green_method_frame(self, master):
+        self.grn_frm = tk.Frame(master, relief='groove', borderwidth=5, bg='green')
+        return self.grn_frm
+    
+    def _select_method(self, event):
+        print(event.widget.get())
+        if event.widget.get() == 'Detect contours':
+            self.cnt_frm.tkraise()
+        else:
+            self.grn_frm.tkraise()
 
 class FvFmFrame(ttk.Frame):
     def __init__(self, master):
